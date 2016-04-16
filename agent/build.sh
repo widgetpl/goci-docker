6d838d8d008d3dd45358cc1887c7cae0a9400469
widgetpl <m.dziedziela@oberthur.com>
2016-04-16 14:42:08 +0200

    added gradle, maven and sbt to agent image

--- agent/build.sh
+++ agent/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-AGENT_VERSION=16.3.0-3183
+AGENT_VERSION=16.3.0-3183-2
 
 docker build -t widgetpl/docker-goci-agent:${AGENT_VERSION} .
 
-docker push widgetpl/docker-goci-agent:${AGENT_VERSION}
+#docker push widgetpl/docker-goci-agent:${AGENT_VERSION}
